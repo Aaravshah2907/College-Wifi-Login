c2e5d6a6538914a7ba475c3bd8270244b142609c
Aarav <aaravshah0011@gmail.com>
2025-04-19 16:37:48 +0530

    Added All Required Codes

---
+++ creds.py
@@ -0,0 +1,20 @@
+'''
+4Tredir: https://fw.bits-pilani.ac.in:8090/login?66c442b857e6c685
+magic: 62ce48b963e632e2
+username: F20231325
+password: F20267822#
+'''
+'''
+4Tredir: https://fw.bits-pilani.ac.in:8090/login?61c54cb0a441f93e
+magic: 62cc4fbba55683de
+username: F20231325
+password: F20267822#
+'''
+'''
+4Tredir: https://fw.bits-pilani.ac.in:8090/login?65ca44b3c1c9b8e2
+magic: 60cb45bfcedc32bc
+username: F20231325
+password: F20267822#
+'''
+USERNAME = 'F20231325'
+PASSWORD = 'F20267822#'
